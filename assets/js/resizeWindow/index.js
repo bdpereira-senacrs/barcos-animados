a3216b30014dfc15247805d5ee21fdf45df7d2a5
Bruno Dahlem <bdpereira@senacrs.com.br>
2025-04-03 16:03:03 -0300

    Ajustes de ratio crop

--- assets/js/resizeWindow/index.js
+++ assets/js/resizeWindow/index.js
@@ -1,13 +1,13 @@
 const proportionScale = (width, height) => {
   const widthScreen = window.visualViewport.width;
   const heightScreen = window.visualViewport.height;
-  const proporcaoHeight = (heightScreen * 100) / height;
-  const proporcaoWidth = (widthScreen * 100) / width;
+  const proporcaoHeight = heightScreen / height;
+  const proporcaoWidth = widthScreen / width;
 
   if (proporcaoHeight < proporcaoWidth) {
-    return [proporcaoHeight / 100, "height", "width"];
+    return [proporcaoHeight, "height", "width"];
   } else {
-    return [proporcaoWidth / 100, "width", "height"];
+    return [proporcaoWidth, "width", "height"];
   }
 };
 
@@ -16,14 +16,12 @@ export const resizeWindow = (selector = ".game") => {
   const node = document.querySelector(selector);
 
   node.style.transform = `scale(${proporcao1920})`;
-  node.style.transformOrigin = `center center`;
+  node.style.transformOrigin = `top`;
 
   let proporcao900;
   if (window.visualViewport.width < 992) {
     const btnsMobile = document.querySelector(".btnsMobile");
     proporcao900 = proportionScale(900, 576)[0];
-    btnsMobile.style.top = `${1080 * proporcao1920}px`;
-    node.style.transformOrigin = `top center`;
   } else {
     proporcao900 = 1;
   }
